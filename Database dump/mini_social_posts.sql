ef295cdef6069a21f836b9beba1f1dcc36ad9ee9
Thien <dongocthien1411@gmail.com>
2026-08-25 08:46:02 +0700

    Update server (post content word only done)

---
+++ Database dump/mini_social_posts.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 8.0.46, for Win64 (x86_64)
+--
+-- Host: localhost    Database: mini_social
+-- ------------------------------------------------------
+-- Server version	8.0.46
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `posts` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `content` text NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `location` varchar(50) DEFAULT NULL,
+  `gradient` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `posts`
+--
+
+LOCK TABLES `posts` WRITE;
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+INSERT INTO `posts` VALUES (1,'Test002','2026-08-25 01:12:27','Đà Lạt','linear-gradient(180deg, #18191a 0%, #242526 50%, #7a1d26 100%)'),(2,'Test001','2026-08-25 01:40:25','HN','linear-gradient(180deg, #18191a 0%, #242526 50%, #7a1d26 100%)');
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-08-25  8:41:55
--- backend/server.js
+++ backend/server.js
@@ -16,25 +16,21 @@ const db = mysql.createPool( {
 
 app.post('/api/posts', async (req,res) => {
     try {
-        const {content} = req.body;
+        const {content, location, gradient} = req.body;
 
         if (!content) {
             return res.status(400).json({message: "Nội dung không được để trống"})
         }
 
-        console.log("Dữ liệu nhận được từ Thunder Client:", req.body);
-        console.log("Biến content lấy ra được là:", content);
-
-        const sql = "INSERT INTO Posts (content) VALUES (?)";
-        const [result] = await db.query(sql, [content]);
+        const sql = "INSERT INTO Posts (content, location, gradient) VALUES (?, ?, ?)";
+        const [result] = await db.query(sql, [content, location, gradient]);
 
         res.status(201).json({
             message: "Đăng bài thành công!",
             postId: result.insertId
     });
 
-}
-    catch (error) {
+    } catch (error) {
         console.error(error);
         res.status(500).json({message:"Lỗi server"});
     }
